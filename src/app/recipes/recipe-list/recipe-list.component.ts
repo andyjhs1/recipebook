d6803464bbceb0210d46906c006edf96d7b11f31
Anand <anand.gupta@yantriks.com>
2018-04-16 10:13:02 -0400

    updated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output , EventEmitter} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,12 +8,17 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 recipes:Recipe[]=[
-new Recipe('This is a Test','This is Test Description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeH3zeK3hjqm3vKCuTKsKAYMSU5TDjA5F3FZVR3aDup3ViZnPArA'),new Recipe('This is a Test','This is Test Description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeH3zeK3hjqm3vKCuTKsKAYMSU5TDjA5F3FZVR3aDup3ViZnPArA')
+new Recipe('This is a Test','This is Test Description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeH3zeK3hjqm3vKCuTKsKAYMSU5TDjA5F3FZVR3aDup3ViZnPArA'),new Recipe('This is another Test','This is another Test Description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeH3zeK3hjqm3vKCuTKsKAYMSU5TDjA5F3FZVR3aDup3ViZnPArA')
 ];
   constructor() { }
 
   ngOnInit() {
   }
 
+    onRecipeSelected(recipe:Recipe){
+        this.recipeWasSelected.emit(recipe);
+    }
 }
